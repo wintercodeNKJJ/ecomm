09ea0a66a3e43929ae225c7feaf42bdef267e047
wintercodeNKJJ <kenfackjordanjunior@gmail.com>
2023-03-15 19:43:54 +0100

    text color

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,10 +6,10 @@ const Banner = () => {
       <div className="p-2 mx-4 my-2 rounded-box grid grid-cols-2 lg:grid-cols-3 gap-2 bg-gray-400 h-full lg:h-96">
         <div className="p-4 flex flex-col justify-between">
           <div className="">
-            <div className="font-light mx-4 text-2xl">
+            <div className="font-light mx-4 text-2xl text-white">
               Best Sold
             </div>
-            <div className="text-7xl md:text-9xl font-bold m-2">
+            <div className="text-7xl md:text-9xl font-bold m-2 text-white">
               Product
             </div>
           </div>
@@ -23,10 +23,10 @@ const Banner = () => {
         </div>
         <div className="flex flex-col justify-end items-end p-4 col-span-2 lg:col-span-1">
           <div className="">
-            <div className="font-bold">
+            <div className="font-bold text-white">
               description title
             </div>
-            <div className="font-light text-justify">
+            <div className="font-light text-justify text-white">
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, tempora. Quidem commodi vitae, eligendi
                 ipsam veritatis necessitatibus, accusantium nam nisi perferendis.
